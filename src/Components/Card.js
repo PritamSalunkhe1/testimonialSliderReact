de5f21e692039449d008d48c419f4bfc09545332
PritamSalunkhe1 <pritamkumar.s.ps@gmail.com>
2023-03-23 13:54:41 +0530

    Initial Commit.

---
+++ src/Components/Card.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
+
+export const Card = (props) => {
+  let review = props.review;
+
+  return (
+    <div className="flex flex-col md:relative">
+      <div className=" top-[-7rem] z-[10] mx-auto ">
+        <img
+          className="aspect-square rounded-full w-[140px] h-[140px] z-[25] outline-indigo-400"
+          src={review.image}
+          alt="profilepicture"
+        />
+      </div>
+
+      <div className="text-center mt-7 ">
+        <p className="tracking-wider font-bold text-2xl capitalize">
+          {review.name}
+        </p>
+        <p className="text-violet-300 uppercase text-sm">{review.job}</p>
+      </div>
+
+      <div className=" text-violet-400 mx-auto mt-5  ">
+        <FaQuoteLeft />
+      </div>
+
+      <div className="text-center mt-4 text-slate-500">{review.text}</div>
+
+      <div className=" text-violet-400 mx-auto mt-5  ">
+        <FaQuoteRight />
+      </div>
+    </div>
+  );
+};
---
+++ src/data.js
@@ -0,0 +1,51 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Pritam Salunkhe",
+    job: "Full Stack Developer @ TCS",
+    image: "https://avatars.githubusercontent.com/u/84986018?v=4",
+    text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+  },
+  {
+    id: 2,
+    name: "Yashprasad Patil",
+    job: "Web Developer @ Sharpner",
+    image:
+      "https://media.licdn.com/dms/image/D4D35AQE6zBpA-MudmQ/profile-framedphoto-shrink_800_800/0/1678383105318?e=1680166800&v=beta&t=2kPh5PyD61slBat5oJFiuX3oI9549Eb3P6sSPTYkN-4",
+    text: "An enthusiastic fresher with highly motivated and focused, persuing bachelor's of engineering degree in Chemical Engineering at Sinhgad college of engineering. Expert in implementation of each step of project. Eager to learn new technologies and methodologies with excellent inter-personal skills.",
+  },
+  {
+    id: 3,
+    name: "Ratnadip Patil",
+    job: "Network Engineer @ Vodafone Idea",
+    image:
+      "https://media.licdn.com/dms/image/C4D03AQE8VRsRU_2gaA/profile-displayphoto-shrink_800_800/0/1651909606637?e=1684972800&v=beta&t=SG9ITrKWP91l8iuEmVGhU_uLNeEzMX4W6eMDM2v6w-o",
+    text: "I am thrilled with the results of our recent marketing campaign, and it wouldn't have been possible without the hard work and dedication of the entire team. Thank you for your exceptional work!",
+  },
+  {
+    id: 4,
+    name: "Parth Jadhav",
+    job: "Software Developer @ Tech Mahindra",
+    image:
+      "https://media.licdn.com/dms/image/C5635AQGmJg2GaUJTSA/profile-framedphoto-shrink_800_800/0/1618587058413?e=1680166800&v=beta&t=dUd3pYCUS_QypdogR_MRB6e4cQYECo2Yke32_QmvNkk",
+    text: "I have worked with many content creators over the years, but none have impressed me as much as this team. They have a knack for crafting compelling and engaging content that resonates with our audience and drives real results. I highly recommend them!",
+  },
+  {
+    id: 5,
+    name: "Tushar Chaudhari",
+    job: "Front-End Developer @ Sharpner",
+    image:
+      "https://media.licdn.com/dms/image/D4D35AQHUbxL5f2m55w/profile-framedphoto-shrink_800_800/0/1674563420781?e=1680166800&v=beta&t=Nx2YE_CVaoxtXt1cZzjvfbWaqOPbsQkyG9vCE1mFqSg",
+    text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+  },
+  {
+    id: 6,
+    name: "Shreyas Chaudhari",
+    job: "Salesforce Developer @ Sharpner",
+    image:
+      "https://media.licdn.com/dms/image/D4D35AQGUr0d89N6I-A/profile-framedphoto-shrink_800_800/0/1675671852313?e=1680166800&v=beta&t=Gkf92mwEIllLd5y_PTayHsn_bfXhbuNhHsoQKO5_Icg",
+    text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+  },
+];
+
+export default reviews;
